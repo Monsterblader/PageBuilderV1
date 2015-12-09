3f1f90a4d28393a66d545b204af1e45cadf99094
Albert Lee <blader_albert@yahoo.com>
2015-12-09 15:55:23 -0800

    Move modules to __reference.

---
+++ __reference/contact-karma/js/en_US.js
@@ -0,0 +1,7 @@
+var lg;
+
+lg = {
+  contactHeader: "Contact Us",
+  contactPhone: "Call us at: ",
+  contactText: "Contact Us"
+};
---
+++ __reference/edd-karma/js/en_US.js
@@ -0,0 +1,10 @@
+var lg;
+
+lg = {
+  exception_eta: "Exception",
+  noETAMsg: "No idea",
+  shippedNoETAMsg: "Unknown",
+  whereIsMyPackage: "",
+  whereIsMyPackageCopy: "Where my package?",
+  whereIsMyPackageMessage: "Tough luck, bruh."
+};
